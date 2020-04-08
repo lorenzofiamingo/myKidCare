6b707d592a86c191a0967334764fa7a7974dd0d8
lorenzofiamingo <l99fiamingo@gmail.com>
2020-04-08 03:01:00 +0200

    .

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+$("#slider").slider({
+    min: 0,
+    max: 100,
+    step: 1,
+    values: [10, 90],
+    slide: function(event, ui) {
+        for (var i = 0; i < ui.values.length; ++i) {
+            $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
+        }
+    }
+});
 
+$("input.sliderValue").change(function() {
+    var $this = $(this);
+    $("#slider").slider("values", $this.data("index"), $this.val());
+    $(".slider-max").slider("values", $this.data("index"), $this.val());
+});
