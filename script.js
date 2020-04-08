b6392ce6ae84344fd70d68ca36d8397129c95b74
lorenzofiamingo <l99fiamingo@gmail.com>
2020-04-08 15:30:53 +0200

    .

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
   $(".toggle").click(function() {
     $(this).toggleClass("active");
-    var el = this;
-    var par = el.parentNode;
-    var next = el.nextSibling;
-    par.removeChild(el);
-    setTimeout(function() {par.insertBefore(el, next);}, 0)
   });
+
+  function hasTouch() {
+    return 'ontouchstart' in document.documentElement ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0;
+  }
+
+  if (hasTouch()) { // remove all the :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
+
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue;
+
+          if (styleSheet.rules[ri].selectorText.match(':hover')) {
+            styleSheet.deleteRule(ri);
+          }
+        }
+      }
+    } catch (ex) {}
+  }
 });
