105ed1d8f1d646b61b93779bfe8906766bfa7ecd
lorenzofiamingo <l99fiamingo@gmail.com>
2020-04-09 20:29:42 +0200

    .

--- script.js
+++ script.js
@@ -27,4 +27,19 @@ $(document).ready(function() {
       }
     } catch (ex) {}
   }
+
+  $(function() {
+    $(".slider-range").slider({
+      range: true,
+      min: 0,
+      max: 100,
+      values: [0, 100],
+      step: 10,
+      slide: function(event, ui) {
+        $(".amount").val("€" + ui.values[0] + " - €" + ui.values[1]);
+      }
+    });
+    $(".amount").val("$" + $(".slider-range").slider("values", 0) +
+      " - $" + $(".slider-range").slider("values", 1));
+  });
 });
